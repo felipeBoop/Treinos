c19e6ab3291f91d28f48cc8ceebd1312073de445
Felipe Eduardo <80724712+felipeBoop@users.noreply.github.com>
2022-05-15 20:54:36 -0300

    Add files via upload

---
+++ cursosJavaScriptOneBit/modulo-JavaScript-logica-1/aula-2-operacoes-basicas/operacoes-basicas.js
@@ -0,0 +1,15 @@
+console.log("==Operações numéricas=="); 
+console.log(5 + 5);
+console.log(5 - 7);
+console.log(5 * 9);
+console.log(5 / 2);
+console.log(5.1 - 0.1);
+console.log(5 % 2);
+console.log("\n==Operacões com String==");
+console.log("Planteta" + "Terra" + " - A concatenação n acrescenta espaço");
+console.log("Planeta " + "Terra");
+console.log("Planeta Terra"[2] + " - Desta forma eu isolo o caractere 'a', posições das strings 0/1/2/3.../n");
+console.log("Planeta Terra".lenght + " - Retorna a quantidade de caracteres");                        
+console.log("Planeta " + "Terra"[3] + " - Podemos juntas operações diferentes aqui ele extrai e depois concatena")
+console.log(("Planeta " + "Terra")[3] + " - devido ao parenteses primeiro ele concatena e depois extrai")
+console.log("Eu vivo ma Terra a " + 30 + "anos" + " - devido a tipagem fraca, ele soma o 30 como string")
