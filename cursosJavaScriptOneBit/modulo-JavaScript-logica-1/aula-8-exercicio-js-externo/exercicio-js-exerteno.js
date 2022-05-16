722f6cb759643d598c2bbeee43bbd9f4add9a632
Felipe Eduardo <80724712+felipeBoop@users.noreply.github.com>
2022-05-16 20:41:12 -0300

    Add files via upload

---
+++ cursosJavaScriptOneBit/modulo-JavaScript-logica-1/aula-8-exercicio-js-externo/exercicio-js-exerteno.js
@@ -0,0 +1,12 @@
+let nPV = prompt("informe o nome da pessoa mais velha: ");
+let iPV = prompt("informe a idade da pessoa mais velha: ");
+let nPN = prompt("Informe o nome da pessoa mais nova: ");
+let iPN = prompt("Informe a idade da pessoa mais velha: ");
+
+let dif = iPV - iPN;
+
+alert("Nome da pessoa mais velha: " + nPV
+     +"\nIdade da pessoa mais velha " + iPV
+     +"\n\nNome da pessoa mais nova: " + nPN
+     +"\nIdade da pessoa mais nova: " + iPN
+     +"\n\nDiferença de idade: " + dif);
