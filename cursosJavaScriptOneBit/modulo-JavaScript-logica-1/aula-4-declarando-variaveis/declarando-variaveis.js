90c788a579db4b0890b5c5bd453ea73df7ef870e
Felipe Eduardo <80724712+felipeBoop@users.noreply.github.com>
2022-05-16 19:31:24 -0300

    Add files via upload

---
+++ cursosJavaScriptOneBit/modulo-JavaScript-logica-1/aula-4-declarando-variaveis/declarando-variaveis.js
@@ -0,0 +1,15 @@
+let spaceShip = "teste";
+console.log(spaceShip);
+spaceShip = "FE helmet";
+console.log(spaceShip);
+
+const testezinho = "testezinho";
+console.log(testezinho);
+//const testezinho = "teste 2 - n vai rolar";
+console.log(testezinho);
+
+let semValor = null;
+let departure = confirm("nave p/ p/ decolar?");
+let depatureConfirm = prompt("qual a nave?");
+console.log(departure);
+console.log(depatureConfirm);
