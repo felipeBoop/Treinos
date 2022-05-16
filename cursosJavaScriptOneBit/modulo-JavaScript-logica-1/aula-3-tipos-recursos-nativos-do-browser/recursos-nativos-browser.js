12863ecbc577b3e6f17861463581f024f22216d7
Felipe Eduardo <80724712+felipeBoop@users.noreply.github.com>
2022-05-16 18:45:32 -0300

    Add files via upload

---
+++ cursosJavaScriptOneBit/modulo-JavaScript-logica-1/aula-3-tipos-recursos-nativos-do-browser/recursos-nativos-browser.js
@@ -0,0 +1,10 @@
+console.log("++== Tipos de console ==++");
+
+console.log("\nteste :)");
+console.info("teste azulzinho :)");
+console.warn("teste laranjinha :)");
+console.error("teste error :)");
+
+alert("Oi sou um alert chato |)");
+confirm("sim ou não?");
+prompt("input aqui???? LOL"); 
