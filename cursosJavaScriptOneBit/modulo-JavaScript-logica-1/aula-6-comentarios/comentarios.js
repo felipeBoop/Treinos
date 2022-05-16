865591c6802c307667ad8f6d502ee4e91b7ecd96
Felipe Eduardo <80724712+felipeBoop@users.noreply.github.com>
2022-05-16 20:13:49 -0300

    Add files via upload

---
+++ cursosJavaScriptOneBit/modulo-JavaScript-logica-1/aula-6-comentarios/comentarios.js
@@ -0,0 +1,10 @@
+/*
+
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Molestias quae iure tempore aperiam repellat possimus ducimus. 
+    Reiciendis ipsa voluptatibus reprehenderit voluptas aut, 
+    voluptatem exercitationem fugit nostrum, provident, labore nam neque!
+
+*/
+
+console.log("tem um comentário aqui?");
