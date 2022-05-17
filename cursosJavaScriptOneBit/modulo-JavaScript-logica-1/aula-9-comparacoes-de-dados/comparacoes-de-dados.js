d73a21733d24b3a42057be05c23e0f5c98950d30
Felipe Eduardo <80724712+felipeBoop@users.noreply.github.com>
2022-05-17 18:43:46 -0300

    Add files via upload

---
+++ cursosJavaScriptOneBit/modulo-JavaScript-logica-1/aula-9-comparacoes-de-dados/comparacoes-de-dados.js
@@ -0,0 +1,17 @@
+let velocity = 80;
+let spaceship = "Golias"
+
+console.log("velocity == 80" + velocity == 80);
+
+console.log("velocity == '80'" + velocity == "80")
+console.log("velocity === '80'" + velocity === "80");
+console.log("spaceship == 'Golias'" + spaceship == "Golias");
+
+console.log("velocity != 70" + velocity != 70);
+console.log("velocity != 80" + velocity != 80);
+console.log("velocity !== '80'" + velocity !== 80);
+
+console.log("velocity > 80" + velocity > 80);
+console.log("velocity > 70" + velocity > 70);
+console.log("spaceship > 'Hercules'" + spaceship > "Hercules");
+console.log("spaceship >= 'Hercules'" + spaceship >= "Hercules");
