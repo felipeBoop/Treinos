caeaf1b406fd9509b774fe2e9d2d20fa0e954071
Felipe Eduardo <80724712+felipeBoop@users.noreply.github.com>
2022-06-21 22:40:51 -0300

    Add files via upload

---
+++ jogos_em_lua/shooter/main.lua
@@ -0,0 +1,81 @@
+function love.load() --roda no inicio do programa.
+    target = {}
+
+    sprites = {}
+    sprites.sky = love.graphics.newImage('sprites/sky.png');
+    sprites.target = love.graphics.newImage('sprites/target.png');
+    sprites.crosshairs = love.graphics.newImage('sprites/crosshairs.png');
+
+    love.mouse.setVisible(false)
+
+    target.x = 300
+    target.y = 300
+    target.radius = 50
+
+    score = 0
+    timer = 0
+    mouseToTarget = 0
+    gameState = 1
+end
+
+function love.update(dt) 
+    if timer > 0 then
+        timer = timer - dt
+    end
+
+    if timer < 0 then
+        timer = 0
+        gameState = 1
+    end
+end
+
+function love.draw()
+    love.graphics.draw(sprites.sky, 0, 0)
+
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.setFont(love.graphics.newFont(30))
+    love.graphics.print("Score: " .. score, 5, 5)
+    love.graphics.print("Time: " .. math.ceil(timer), 200, 5) --ceil number up of the next, floor number down for the before number.
+
+    if gameState == 1 then
+        love.graphics.printf("Clique em qualquer lugar para começar!", 
+                             0, 
+                             love.graphics.getHeight() / 2,
+                             love.graphics.getWidth(),
+                             "center"
+                            )
+    end
+
+    if gameState == 2 then
+        love.graphics.draw(sprites.target, target.x - target.radius, target.y - target.radius)
+    end
+
+    love.graphics.draw(sprites.crosshairs, love.mouse.getX() -20, love.mouse.getY() - 20)
+    
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    if gameState == 2 then
+        local mouseToTarget = distanceBetween(x, y, target.x, target.y)
+        if mouseToTarget < target.radius then
+            if button == 1 then
+                score = score + 1
+            elseif button == 2 then
+                score = score + 2
+                timer = timer - 1
+            end
+            target.x = math.random(target.radius, love.graphics.getWidth() - target.radius)
+            target.y = math.random(target.radius, love.graphics.getHeight() - target.radius)
+        elseif score > 0 then
+            score = score - 1
+        end
+    elseif button == 1 and gameState == 1 then
+        gameState = 2
+        timer = 10
+        score = 0
+    end
+end
+
+function distanceBetween(x1, y1, x2, y2)
+   return math.sqrt( (x2 - x1)^2 + (y2 - y1)^2 ) 
+end
